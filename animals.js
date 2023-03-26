ec21c4030e782bb9cba320b94005331c926d6f89
Lucky69-XD <128928140+Lucky69-XD@users.noreply.github.com>
2023-03-26 12:01:58 +0530

    Add files via upload

--- animals.js
+++ animals.js
@@ -14,28 +14,129 @@ function search_event() {
 	}
 }
 
-const event = [
+const events = [
 	{
-		title: "Raj and DK Filmmaking workshop"	
+	  title: "Cricket",
+	  summary: "Play cricket branch wise",
+	  description: "Play cricket branch wise and win prices......",
+	  venue: "Ground",
+	  link:"events/cricket.html"
 	},
 	{
-		title: "Karoke and silent disco"	
+	  title: "Volley Ball",
+	  summary: "Play Volley branch wise",
+	  description: "Play Volley branch wise and win prices......",
+	  venue: "Ground",
 	},
 	{
-		title: "Mood indigo got talent"	
+	  title: "Youth Parliment",
+	  summary: "Parliment competetion branch wise",
+	  description: "Parliment competetion branch wise and win prices......",
+	  venue: "Academic complex",
 	},
 	{
-		title: "Salsa and garba workshop"
+	  title: "Watch Movie",
+	  summary: "Watch Movie in the campus",
+	  description: "Watch Movie in the campus and enjoy life ......",
+	  venue: "Cinema Hall",
 	},
 	{
-		title: "Chereonite"
+	  title: "Mother Tongue day event",
+	  summary: "Mother Tongue day competetion branch wise",
+	  description:
+		"Mother Tongue day competetion branch wise and win prices......",
+	  venue: "Academic classroom",
 	},
 	{
-		title
+	  title: "Music Night",
+	  summary: "Music Night with Singer ABC",
+	  description: "Music Night with Singer ABC ans enjoy life......",
+	  venue: "Stadium",
+	},
+	{
+	  title: "Tech Work",
+	  summary: "Music Night with Singer ABC",
+	  description: "Music Night with Singer ABC ans enjoy life......",
+	  venue: "Stadium",
+	},
+	{
+	  title: "Movie buzz",
+	  summary: "Music Night with Singer ABC",
+	  description: "Music Night with Singer ABC ans enjoy life......",
+	  venue: "Stadium",
+	},
+	{
+	  title: "Meet the Freshers",
+	  summary: "Music Night with Singer ABC",
+	  description: "Music Night with Singer ABC ans enjoy life......",
+	  venue: "Stadium",
+	},
+	{
+	  title: "Motivational Speaker event",
+	  summary: "Motivational Speaker event with Singer ABC",
+	  description: "Motivational Speaker event with ABC ans answers life q......",
+	  venue: "Stadium",
+	},
+  ];
+  
+  setTimeout(() => {
+	// for (let i = 0; i < events.length; i++) {
+	//   addEvent(events[i]);
+	// }
+  
+	events.map((e) => {
+	  addEvent(e);
+	});
+  }, 0);
+  
+  const addEvent = (e) => {
+	// create image
+  
+	const eImg = document.createElement("img");
+	eImg.setAttribute(
+	  "src",
+	  "/images/event.jpg"
+	);
+	eImg.setAttribute("alt", "some image");
+  
+	const iDiv = document.createElement("div");
+	const ih3 = document.createElement("h3");
+	const ip = document.createElement("p");
+	const ia = document.createElement("a");
+  
+	ip.innerText = e.summary;
+	ih3.innerText = e.title;
+	ia.innerText = "Register Now";
+	ia.setAttribute("href",e.link);
+  
+	iDiv.appendChild(ih3);
+	iDiv.appendChild(ip);
+	iDiv.append(ia);
+	//   outer div
+	const oDiv = document.createElement("div");
+	oDiv.appendChild(eImg);
+	oDiv.appendChild(iDiv);
+  
+	// Add classes to image
+	oDiv.classList.add("inner");
+	ia.classList.add("link-color");
+	document.querySelector(".outer").appendChild(oDiv);
+  };
+  function searchFunction() {
+	// Declare variables
+	var input, filter, ul, li, a, i;
+	input = document.getElementById('searchInput');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("list");
+	li = ul.getElementsByTagName('li');
+  
+	// Loop through all list items and hide those that don't match the search query
+	for (i = 0; i < li.length; i++) {
+	  a = li[i].getElementsByTagName("a")[0];
+	  if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+		li[i].style.display = "";
+	  } else {
+		li[i].style.display = "none";
+	  }
 	}
-]
-setTimeout(() => {
-	for (let i=0;i< event.lenth;i++){
-		addEvent(event[i])
-	}
-},0);
+  }
